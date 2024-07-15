19b23f058f3330b4776b3c7c04bf46df7d8fff98
Ghufran Ullah <ghufran.chaudary12@gmail.com>
2024-07-16 02:24:49 +0500

    added Checked In Functionality

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -94,12 +94,13 @@ export class ChartsComponent {
 
   ngOnInit() {
     const userData = this.authService.gettoken();
-    console.log(userData);
+    //console.log(userData);
 
     if (userData) {
       this.adminService.getSingleUserAllAttendById(userData.id).subscribe(
         (data) => {
           this.singleuserAllAttendances = data;
+          console.log(this.singleuserAllAttendances);
           this.updateChartData();
         },
         (error) => {
@@ -140,7 +141,7 @@ export class ChartsComponent {
     return formattedTime;
   }
 
-  missingYValues = () => {
+ /* missingYValues = () => {
     const options = { ...this.options, data: [...this.getData()] };
     const data = this.getData();
     data[2].high = undefined;
@@ -162,152 +163,7 @@ export class ChartsComponent {
     options.data = this.getData();
     this.options = options;
   };
+*/
+
 }
 
-// import { Component, OnInit } from '@angular/core';
-// import { AgCharts } from 'ag-charts-angular';
-// import { AgChartOptions } from 'ag-charts-community';
-// import 'ag-charts-enterprise';
-// import { AuthService } from '../../services/auth.service';
-// import { AdminService } from '../../services/admin.service';
-
-// @Component({
-//   selector: 'app-charts',
-//   standalone: true,
-//   imports: [AgCharts],
-//   templateUrl: './charts.component.html',
-//   styleUrls: ['./charts.component.css'],
-// })
-// export class ChartsComponent implements OnInit {
-//   public options: AgChartOptions;
-//   public singleuserAllAttendances: any = [];
-
-//   constructor(
-//     public authService: AuthService,
-//     public adminService: AdminService
-//   ) {
-//     this.options = {
-//       data: this.getData(),
-//       title: {
-//         text: 'Employee Attendances',
-//       },
-//       series: [
-//         {
-//           type: 'range-bar',
-//           xKey: 'date',
-//           xName: 'Date',
-//           yLowKey: 'low',
-//           yHighKey: 'high',
-//         },
-//       ],
-//       axes: [
-//         {
-//           type: 'number',
-//           position: 'left',
-//         },
-//         {
-//           type: 'time',
-//           position: 'bottom',
-//           nice: false,
-//           crosshair: {
-//             enabled: false,
-//           },
-//         },
-//       ],
-//     };
-//   }
-
-//   ngOnInit() {
-//     const userData = this.authService.gettoken();
-//     console.log(userData);
-
-//     if (userData) {
-//       this.adminService.getSingleUserAllAttendById(userData.id).subscribe(
-//         (data) => {
-//           this.singleuserAllAttendances = data;
-//           console.log('Fetched data:', this.singleuserAllAttendances);
-//           // Update chart data with fetched data
-//           this.updateChartData();
-//         },
-//         (error) => {
-//           console.error('Error fetching data:', error);
-//         }
-//       );
-//     }
-//   }
-
-//   updateChartData() {
-//     const chartData = this.singleuserAllAttendances.map((attendance:any) => ({
-//       date: new Date(attendance.attendanceDate), // Assuming 'attendanceDate' is in ISO string format
-//       low: new Date(attendance.checkInTime).getTime(), // Convert to milliseconds
-//       high: new Date(attendance.checkOutTime).getTime(), // Convert to milliseconds
-//     }));
-
-//     const options = { ...this.options, data: chartData };
-//     this.options = options;
-//   }
-
-//   // missingYValues = () => {
-//   //   const options = { ...this.options, data: [...this.getData()] };
-//   //   const data = this.getData();
-//   //   data[2].high = undefined;
-//   //   data[5].low = undefined;
-//   //   options.data = data;
-//   //   this.options = options;
-//   // };
-
-//   // missingXValue = () => {
-//   //   const options = { ...this.options, data: [...this.getData()] };
-//   //   const data = this.getData();
-//   //   data[6].date = undefined;
-//   //   options.data = data;
-//   //   this.options = options;
-//   // };
-
-//   // reset = () => {
-//   //   const options = { ...this.options, data: [...this.getData()] };
-//   //   options.data = this.getData();
-//   //   this.options = options;
-//   // };
-
-//   getData(): any[] {
-//     return [
-//       { date: new Date(2022, 10, 1), high: 800, low: 1700 },
-//       { date: new Date(2022, 10, 2), high: 900, low: 1800 },
-//       { date: new Date(2022, 10, 3), high: 830, low: 1600 },
-//       { date: new Date(2022, 10, 4), high: 800, low: 1630 },
-//       { date: new Date(2022, 10, 5), high: 600, low: 1700 },
-//       { date: new Date(2022, 10, 6), high: 800, low: 1700 },
-//       { date: new Date(2022, 10, 7), high: 800, low: 1700 },
-//       { date: new Date(2022, 10, 8), high: 600, low: 1500 },
-//       { date: new Date(2022, 10, 9), high: 700, low: 1600 },
-//       { date: new Date(2022, 10, 10), high: 600, low: 1500 },
-//       { date: new Date(2022, 10, 11), high: 700, low: 1900 },
-//       { date: new Date(2022, 10, 12), high: 800, low: 1700 },
-//       { date: new Date(2022, 10, 13), high: 800, low: 1700 },
-//       { date: new Date(2022, 10, 14), high: 900, low: 1800 },
-//       { date: new Date(2022, 10, 15), high: 830, low: 1600 },
-//       { date: new Date(2022, 10, 16), high: 800, low: 1630 },
-//       { date: new Date(2022, 10, 17), high: 600, low: 1700 },
-//       { date: new Date(2022, 10, 18), high: 800, low: 1700 },
-//       { date: new Date(2022, 10, 19), high: 800, low: 1700 },
-//       { date: new Date(2022, 10, 20), high: 600, low: 1500 },
-//       { date: new Date(2022, 10, 21), high: 700, low: 1600 },
-//       { date: new Date(2022, 10, 22), high: 600, low: 1500 },
-//       { date: new Date(2022, 10, 23), high: 700, low: 1900 },
-//       { date: new Date(2022, 10, 24), high: 800, low: 1700 },
-//       { date: new Date(2022, 10, 25), high: 800, low: 1700 },
-//       { date: new Date(2022, 10, 26), high: 900, low: 1800 },
-//       { date: new Date(2022, 10, 27), high: 830, low: 1600 },
-//       { date: new Date(2022, 10, 28), high: 800, low: 1630 },
-//       { date: new Date(2022, 10, 29), high: 600, low: 1700 },
-//       { date: new Date(2022, 10, 30), high: 800, low: 1700 },
-//       { date: new Date(2022, 11, 1), high: 800, low: 1700 },
-//       { date: new Date(2022, 11, 2), high: 600, low: 1500 },
-//       { date: new Date(2022, 11, 3), high: 700, low: 1600 },
-//       { date: new Date(2022, 11, 4), high: 600, low: 1500 },
-//       { date: new Date(2022, 11, 5), high: 700, low: 1900 },
-//       { date: new Date(2022, 11, 6), high: 800, low: 1700 },
-//     ];
-//   }
-// }
--- src/app/components/single-user-salaries/single-user-salaries.component.ts
+++ src/app/components/single-user-salaries/single-user-salaries.component.ts
@@ -2,20 +2,97 @@ import { Component } from '@angular/core';
 import { AdminService } from '../../services/admin.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { AgCharts } from 'ag-charts-angular';
+import { AgChartOptions } from 'ag-charts-community';
+import 'ag-charts-enterprise';
+
 
 @Component({
   selector: 'app-single-user-salaries',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,AgCharts],
   templateUrl: './single-user-salaries.component.html',
   styleUrl: './single-user-salaries.component.css'
 })
 export class SingleUserSalariesComponent {
+  public options: AgChartOptions;
 
-  singleuserAllSalaries: any = [];
-  singleuserAllAttendances: any = [];
+ public singleuserAllSalaries: any = [];
+ public singleuserAllAttendances: any = [];
 
-  constructor(private adminService: AdminService,private route: ActivatedRoute) {}
+  constructor(private adminService: AdminService,private route: ActivatedRoute) {
+    this.options = {
+      data: this.getData(),
+      title: {
+        text: 'Employee Attendances',
+      },
+      series: [
+        {
+          type: 'range-bar',
+          xKey: 'date',
+          xName: 'Date',
+          yLowKey: 'low',
+          yHighKey: 'high',
+        },
+      ],
+      axes: [
+        {
+          type: 'number',
+          position: 'left',
+        },
+        {
+          type: 'time',
+          position: 'bottom',
+          nice: false,
+          crosshair: {
+            enabled: false,
+          },
+        },
+      ],
+    };
+  }
+
+
+  getData(): any[] {
+    return [
+      { date: new Date(2022, 10, 1), high: 800, low: 1700 },
+      { date: new Date(2022, 10, 2), high: 900, low: 1800 },
+      { date: new Date(2022, 10, 3), high: 830, low: 1600 },
+      { date: new Date(2022, 10, 4), high: 800, low: 1630 },
+      { date: new Date(2022, 10, 5), high: 600, low: 1700 },
+      { date: new Date(2022, 10, 6), high: 800, low: 1700 },
+      { date: new Date(2022, 10, 7), high: 800, low: 1700 },
+      { date: new Date(2022, 10, 8), high: 600, low: 1500 },
+      { date: new Date(2022, 10, 9), high: 700, low: 1600 },
+      { date: new Date(2022, 10, 10), high: 600, low: 1500 },
+      { date: new Date(2022, 10, 11), high: 700, low: 1900 },
+      { date: new Date(2022, 10, 12), high: 800, low: 1700 },
+      { date: new Date(2022, 10, 13), high: 800, low: 1700 },
+      { date: new Date(2022, 10, 14), high: 900, low: 1800 },
+      { date: new Date(2022, 10, 15), high: 830, low: 1600 },
+      { date: new Date(2022, 10, 16), high: 800, low: 1630 },
+      { date: new Date(2022, 10, 17), high: 600, low: 1700 },
+      { date: new Date(2022, 10, 18), high: 800, low: 1700 },
+      { date: new Date(2022, 10, 19), high: 800, low: 1700 },
+      { date: new Date(2022, 10, 20), high: 600, low: 1500 },
+      { date: new Date(2022, 10, 21), high: 700, low: 1600 },
+      { date: new Date(2022, 10, 22), high: 600, low: 1500 },
+      { date: new Date(2022, 10, 23), high: 700, low: 1900 },
+      { date: new Date(2022, 10, 24), high: 800, low: 1700 },
+      { date: new Date(2022, 10, 25), high: 800, low: 1700 },
+      { date: new Date(2022, 10, 26), high: 900, low: 1800 },
+      { date: new Date(2022, 10, 27), high: 830, low: 1600 },
+      { date: new Date(2022, 10, 28), high: 800, low: 1630 },
+      { date: new Date(2022, 10, 29), high: 600, low: 1700 },
+      { date: new Date(2022, 10, 30), high: 800, low: 1700 },
+      { date: new Date(2022, 11, 1), high: 800, low: 1700 },
+      { date: new Date(2022, 11, 2), high: 600, low: 1500 },
+      { date: new Date(2022, 11, 3), high: 700, low: 1600 },
+      { date: new Date(2022, 11, 4), high: 600, low: 1500 },
+      { date: new Date(2022, 11, 5), high: 700, low: 1900 },
+      { date: new Date(2022, 11, 6), high: 800, low: 1700 },
+    ];
+  }
 
   ngOnInit() {
     const UserId = this.route.snapshot.paramMap.get('id');
@@ -36,7 +113,8 @@ export class SingleUserSalariesComponent {
     this.adminService.getSingleUserAllSalById(UserId).subscribe(
       (data: any) => {
         this.singleuserAllSalaries = data;
-        console.log(this.singleuserAllSalaries);
+        //console.log(this.singleuserAllSalaries);
+        
       },
       (err) => {
         console.log('Error fetching users:', err);
@@ -48,7 +126,7 @@ export class SingleUserSalariesComponent {
     this.adminService.getSingleUserAllAttendById(UserId).subscribe(
       (data: any) => {
         this.singleuserAllAttendances = data;
-        console.log(this.singleuserAllAttendances);
+        this.updateChartData();
       },
       (err) => {
         console.log('Error fetching users:', err);
@@ -78,4 +156,37 @@ export class SingleUserSalariesComponent {
       }
     );
   }
+
+  updateChartData() {
+    const chartData = this.singleuserAllAttendances.map((attendance: any) => {
+      console.log(attendance)
+      // Format checkInTime and checkOutTime as required
+      const attendanceDate = new Date(attendance.attendanceDate);
+      const checkInTime = this.formatTime(attendance.checkInTime);
+      const checkOutTime = this.formatTime(attendance.checkOutTime);
+      return {
+        date: attendanceDate,
+        low: checkInTime,
+        high: checkOutTime,
+      };
+    });
+
+    const options = { ...this.options, data: chartData };
+    this.options = options;
+  }
+
+  formatTime(dateTimeString: string): number {
+    // Create a new Date object from the datetime string
+    const dateTime = new Date(dateTimeString);
+
+    // Extract the time components
+    const hours = dateTime.getHours();
+    const minutes = dateTime.getMinutes();
+
+    // Format the time as HHmm (e.g., 8:00 -> 800, 9:00 -> 900)
+    const formattedTime = hours * 100 + minutes;
+
+    return formattedTime;
+  }
+
 }
